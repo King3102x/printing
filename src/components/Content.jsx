d8d1761e138e6be01add76b15c2666a76a3cb9a8
King3102x <132570585+King3102x@users.noreply.github.com>
2024-02-17 15:33:29 +0530

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -27,24 +27,24 @@ export default function Content() {
         <div className="bg-gray-100 p-6 rounded-lg">
           <Image className="h-40 rounded w-full object-cover object-center mb-6" src="/img/imagee.png" width={400} height={400} alt="content"/>
           <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-white font-medium title-font mb-4">img name</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-4">Gravure Printing</h2>
+          <p className="leading-relaxed text-base">Involves engraving an image onto a cylinder, which is then used to transfer ink to the substrate.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <Image className="h-40 rounded w-full object-cover object-center mb-6" src="/img/imagee.png" width={400} height={400} alt="content"/>
           <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-white font-medium title-font mb-4">img name</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-4">Laser Printing</h2>
+          <p className="leading-relaxed text-base">Utilizes a laser beam to create an image on a photoreceptor drum or belt, which is then transferred and fused onto the printing surface.</p>
         </div>
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">    
           <Image className="h-40 rounded w-full object-cover object-center mb-6" src="/img/imagee.png" width={400} height={400} alt="content"/>
           <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 className="text-lg text-white font-medium title-font mb-4">img Name</h2>
-          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <h2 className="text-lg text-white font-medium title-font mb-4">3D Printing</h2>
+          <p className="leading-relaxed text-base">Builds three-dimensional objects layer by layer from digital files. It's used in various industries for prototyping and etc.</p>
         </div>
       </div>
     </div>
